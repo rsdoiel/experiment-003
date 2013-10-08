d562fa40b99ac2404ebfa1549ee5d750d56330ff
R. S. Doiel <rsdoiel@gmail.com>
2013-10-08 15:21:34 -0700

    Create http_test.js

---
+++ www/js/http_test.js
@@ -0,0 +1,26 @@
+/**
+ * http_test.js - tests for the http object.
+ */
+/*jslint browser: true, indent: 4 */
+(function (global) {
+    "use strict";
+    var http = global.http,
+        test_http = document.getElementById("http-test-results");
+
+    function print(elem, msg) {
+        elem.textContent += msg + "\n";
+    }
+
+    function httpTests() {
+        if (typeof http === "object") {
+            print(test_http, "OK, http object found");
+        } else {
+            print(test_http, "ERROR: http object missing");
+            return;
+        }
+        print(test_http, "ERROR http.addRequestHeader() tests not implemented");
+        print(test_http, "ERROR http.resetRequestHeader() tests not implemented");
+        print(test_http, "ERROR http.GET() tests not implemented");
+    }
+    print(test_http, "ERROR: http tests not implemented.");
+}(this));
